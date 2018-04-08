f992d36d02d78a89ba33633f7c1b61ffe977e1d5
Joshua Send <joshuasend1@gmail.com>
2018-04-08 19:07:24 +0100

    Implement majority of class for constant curvature with working transforms

--- src/linefollow_gazebo/scripts/ConstantCurvaturePath.py
+++ src/linefollow_gazebo/scripts/ConstantCurvaturePath.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-
-import math
 import numpy as np
-from numpy import pi as PI
 import rospy
 import tf2_py as tf2
 import tf2_ros
-import tf_conversions.transformations as tf_transformations
+import tf_conversions
 
-from sensor_msgs.msg import Imu
-from prius_msgs.msg import Control
-from nav_msgs.msg import Odometry
+tf_transformations = tf_conversions.transformations
 
 import tf2_geometry_msgs
 from geometry_msgs.msg import PointStamped, TransformStamped
@@ -30,15 +24,19 @@ class ConstantCurvaturePath(object):
     """
 
     def __init__(self, curvature, time_parametrized=True):
-        self.init_point = np.array([0.0, 0.0, 0.0)]
+        self.init_point = np.array([0.0, 0.0, 0.0])
         # TODO check this is the right Quaternion for x-axis aligned orientation
         self.init_orientation = tf_transformations.quaternion_from_euler(0, 0, 0)
         self.curvature = curvature
         if self.curvature != 0.0:
-            self.r = 1/self.curvature
+            self.r = np.abs(1/self.curvature)
         self.time_parametrized = time_parametrized
         
         self.start_time = 0.0
+
+        # to avoid repeatedly computing curve points if ever needed
+        # save them as PointStamped and reuse
+        self.concrete_curve_cache = None
 
     def begin(self, current_time, current_position, current_orientation, speed):
         # translate and rotate path centered at (0,0,0) and x-axis aligned to match current_pose and current_orientation
@@ -71,6 +69,27 @@ class ConstantCurvaturePath(object):
       
         self.start_time = current_time
 
+        self.concrete_curve_cache = None
+
+    def get_concrete_curve_points(self, start_time, end_time, speed=1.0, steps=100, return_type='numpy')
+        curve_points_stamped = self.get_points_transformed(start_time=start_time,
+                                                           end_time=end_time,
+                                                           speed=speed,
+                                                           transform=self.transform,
+                                                           steps=steps)
+        self.concrete_curve_cache = curve_points_stamped
+
+        if return_type == 'numpy':
+            points = np.array([np.array(p.point.x, p.point.y, p.point.z) for p in curve_points_stamped])
+            return points
+        elif return_type == 'Point':
+            rospy.logerr("Point type concrete path not implemented")
+            raise NotImplementedException("Point type not implemented")
+        elif return_type == 'PointStamped'):
+            return curve_points_stamped
+        else:
+            raise UnknownPathTypeException("Unknown return type requested: " + return_type)
+
 
     # for debugging, test transformed paths 
     def get_points_transformed(self, start_time, end_time, transform, speed=1.0, steps=100):
@@ -82,7 +101,7 @@ class ConstantCurvaturePath(object):
 
 
     # as per ROS/Gazebo standards, speed is m/s
-    def get_abstract_curve_points(self, start_time, end_time, speed=1.0, steps=100, return_type='numpy');
+    def get_abstract_curve_points(self, start_time, end_time, speed=1.0, steps=100, return_type='numpy'):
         """ 
         Computes a set of `steps` points representing the time-parametrized curve
         from t=`start_time` to t=`end_time` traversed at a given constant speed
@@ -90,10 +109,10 @@ class ConstantCurvaturePath(object):
         returns as a numpy array of numpy arrays, Point, or PointStamped
         """
 
-        abstract_path = np.empty(steps)
+        abstract_path =[] 
         dt = (float(end_time) - start_time) / steps
-        for (t,i) in enumerate(np.arange(start_time, end_time, dt)):
-            abstract_path[i] = self.get_point(t, speed, return_type=return_type)
+        for t in np.arange(start_time, end_time, dt):
+            abstract_path.append(self.get_point(t, speed, return_type=return_type))
         
         return abstract_path
         
@@ -104,7 +123,7 @@ class ConstantCurvaturePath(object):
         dt = t - self.start_time
         if self.curvature == 0.0:
             # straight line along x axis-from start point
-            pt = self.init_point + np.array([dt*speed, 0,0, 0.0)]
+            pt = self.init_point + np.array([dt*speed, 0.0, 0.0])
 
         else:
             # get coefficient for within x(t) = R*cos(a*t), y(t) = R*sin(a*t)
@@ -112,8 +131,8 @@ class ConstantCurvaturePath(object):
             # works out to a = sqrt(speed)/R
             # => a = sqrt(speed) * curvature
             a = np.sqrt(speed) * self.curvature
-            dx = self.r * np.cos(a * (dt - np.pi/2)) # subtract pi/2 to start curve at x = 0 and increasing
-            dy = self.r * np.sin(a * (dt - np.pi/2)) + self.r # add self.r to start at (x,y) = (0,0) and both increasing
+            dx = self.r * np.cos(a * dt - np.pi/2) # subtract pi/2 to start at x = 0 and increasing
+            dy = self.r * np.sin(a * dt - np.pi/2) + (self.r) # add radius to offset to (x,y) = (0,0) and both increasing (if positive curvature)
             dz = 0.0
             pt = self.init_point + np.array([dx, dy, dz])
 
@@ -125,54 +144,42 @@ class ConstantCurvaturePath(object):
             return PointStamped(point=Point(*pt))
         else:
             rospy.logerr("Unknown return type: " + return_type)
-            raise TypeError("Uknown requested point return type: " + return_type) 
+            raise UnknownPathTypeException("Unknown requested point return type: " + return_type) 
 
-class Line_Follow(object):
-    def __init__(self, positioning=None):
-        rospy.loginfo("Create Line_Follow object!")
-        rospy.Subscriber("/imu", Imu, self.on_imu) 
-        self.rate = rospy.get_param("~rate", 20.0)
-        self.period = 1.0 / self.rate
 
-        self.positioning = positioning
 
-        self.started = True
-        self.timer = rospy.Timer(rospy.Duration(self.period), self.update)
-
-    def on_imu(self, imu_msg):
-        rospy.loginfo_throttle(10, str(imu_msg.linear_acceleration))
-        self.last_imu = imu_msg
+    def error(self, position, speed, time, error_type='time_error'):
         
+        if error_type='time_error':
+            return self.time_error(position, time, speed)
 
-    def update(self):
-        if not self.started:
-            return
-        # geti map position from either true or calculated position (doesn't matter for testing)
-        pose = self.positioning.get_pose()
-        position = pose.pose.pose.position
+        elif error_type='crosstrack':
+            # calculate closest point on entirety of path
+            # BAD for two reasons:
+            # 1) may be so far off path we've approached another point and then start tracking in the opposite direction
+            # 2) direction agnostic (eg just following closest points while moving)
+            # 3) computationally expensive
+            return self.crosstrack_error(self, position)
 
-        
-        
-        
-        prius_msg = Control(shift_gears=3, throttle=
-        
+        else:
+            raise UnknownErrorTypeException("Unkown error metric: " + error_type)
 
-class True_Position(object):
-    def __init__(self):
-        rospy.loginfo("Create True Position following")
-        rospy.Subscriber("/base_pose_ground_truth", Odometry, self.on_odom)
-        self.last_post = None
+    def time_error(self, position, time):
+        target_point = self.get_point(time, speed) 
 
-    def on_odom(self, odom_msg):
-        rospy.loginfo_throttle(10, odom_msg)
-        self.last_pose = odom_msg
+        # TODO implement some error as a function of current position and target position
+    
+    def crosstrack_error(self, position):
+        if self.concrete_curve_cache == None
+            self.get_concrete_curve_points() # updates cache internally
 
-    def get_pose(self):
-        return self.last_pose
+        # TODO implement crosstrack distance as error
 
 
-if __name__ == "__main__":
-    rospy.init_node("line_follow_py")
-    positioning = True_Position()
-    line_follow = Line_Follow(positioning=positioning)    
-    rospy.spin()
+class UnknownErrorTypeException(Exception):
+    pass
+class UnknownPathTypeException(Exception):
+    pass
+class NotImplementedException(Exception):
+    def __init__(self, msg):
+        super(NotImplementedException, self).__init__("Not Implemented!" + msg)
