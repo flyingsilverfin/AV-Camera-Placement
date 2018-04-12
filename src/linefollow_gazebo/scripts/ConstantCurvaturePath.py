started work on Camera model - needs testing. Fixed bad bug in helper function for quaternion * vector. Debugging PID line tracking in progress - new error function

src/linefollow_gazebo/scripts/ConstantCurvaturePath.py
@@ -31,7 +31,7 @@ class ConstantCurvaturePath(object):
         if self.curvature != 0.0:
             self.r = np.abs(1/self.curvature)
         self.time_parametrized = time_parametrized
-        
+       
         self.tracking_start_time = 0.0
 
         # to avoid repeatedly computing curve points if ever needed

src/linefollow_gazebo/scripts/helper.py
@@ -23,13 +23,13 @@ def quat_mult_quat(quat1, quat2):
     return tf_conversions.transformations.quaternion_multiply(quat1, quat2)
 
 def quat_mult_point(quat, vec):
-    vec_quat = np.array([0, vec[0], vec[1], vec[2]])
+    vec_quat = np.array([vec[0], vec[1], vec[2], 0])
     q_conj = tf_conversions.transformations.quaternion_conjugate(quat)
 
     # rotation = q*r*q_conj
     a = quat_mult_quat(quat, vec_quat)
     rotated = quat_mult_quat(a, q_conj)
-    return rotated[1:]
+    return rotated[:3]
 
 def normalize(vec):
     return vec/np.linalg.norm(vec)