9420f1c68866ec8d1790a7e9d3e6cf8abac73d57
Joshua Send <joshuasend1@gmail.com>
2018-05-21 09:27:47 +0100

    working on eval

--- run_multiple.bash
+++ run_multiple.bash
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-#python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_centercam_pos1.json --repeats 100 --out evaluation/parallel_simulation/gen --continue
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/calibration_circuit.json --repeats 20 --out evaluation/parallel_simulation/gen --continue
 
-#python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_centercam_pos2.json --repeats 100 --out evaluation/parallel_simulation/gen --continue
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/calibration_circuit_mirror.json --repeats 20 --out evaluation/parallel_simulation/gen --continue
 
-python -m evaluation.parallel_simulation.start_parallel --nparallel 1 --config evaluation/parallel_simulation/defs/straight_road_80m_centercam_pos3.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_centercam_pos1.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_centercam_pos2.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_centercam_pos3.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_halfendcam_pos1.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_halfendcam_pos2.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_halfendcam_pos3.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_halfstartcam_pos1.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_halfstartcam_pos2.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
+
+python -m evaluation.parallel_simulation.start_parallel --nparallel 2 --config evaluation/parallel_simulation/defs/straight_road_80m_halfstartcam_pos3.json --repeats 1000 --out evaluation/parallel_simulation/gen --continue
